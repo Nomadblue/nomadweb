b1e2914bde041ab653b605088a261c3afd6feed1
Hector Garcia <hector@nomadblue.com>
2013-03-23 10:46:10 -0300

    Added contact page

--- nomadweb/urls.py
+++ nomadweb/urls.py
@@ -3,13 +3,14 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 
-from website.views import HomePageView
+from website.views import NomadPageView
 
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    url(r'^$', HomePageView.as_view(), name='home'),
+    url(r'^$', NomadPageView.as_view(), name='home'),
+    url(r'^contact/$', NomadPageView.as_view(template_name='contact.html'), name='contact'),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- website/views.py
+++ website/views.py
@@ -1,9 +1,9 @@
 from django.views.generic.base import TemplateView
 
 
-class HomePageView(TemplateView):
+class NomadPageView(TemplateView):
     template_name = "home.html"
     def get_context_data(self, **kwargs):
-        context = super(HomePageView, self).get_context_data(**kwargs)
+        context = super(NomadPageView, self).get_context_data(**kwargs)
         return context
 
