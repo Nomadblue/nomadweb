84a2cd87844e54ab56689e8c467aae553aaa9199
Hector Garcia <hector@nomadblue.com>
2013-03-24 14:06:31 -0300

    Added view to subscribe to newsletter

--- nomadweb/urls.py
+++ nomadweb/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 
-from website.views import NomadPageView
+from website.views import NomadPageView, SubscriberCreateView
 
 admin.autodiscover()
 
@@ -11,6 +11,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', NomadPageView.as_view(), name='home'),
     url(r'^contact/$', 'website.views.contact', name='contact'),
+    url(r'^subscribe/$', SubscriberCreateView.as_view(), name='subscribe'),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- website/views.py
+++ website/views.py
@@ -1,6 +1,10 @@
 from django.views.generic.base import TemplateView
+from django.views.generic import CreateView
 from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse_lazy
 from django.contrib import messages
+
+from propaganda.models import Subscriber
 
 from contact.forms import ContactForm
 
@@ -20,9 +24,13 @@ def contact(request):
             form.save()
             messages.success(request, "Thanks! We will contact you shortly.")
             return redirect('home')
-
-        # Show system error msg
         messages.error(request, "You must complete all required fields")
     ctxt['form'] = form
     return render(request, template, ctxt)
+
+
+class SubscriberCreateView(CreateView):
+    model = Subscriber
+    template_name = 'subscriber_form.html'
+    success_url = reverse_lazy('home')
 
