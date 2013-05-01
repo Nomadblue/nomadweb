e2f6b9bb2e76f9c4eace5abf00daa573ef4f6b1c
Hector Garcia <hector@nomadblue.com>
2013-04-30 22:21:39 -0400

    Added jobs section

--- nomadweb/urls.py
+++ nomadweb/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 
 from website.views import NomadPageView, SubscriberCreateView
 from website.feeds import LatestEntries, LatestEntriesDjango, LatestEntriesPython
@@ -12,6 +13,8 @@ MULTIBLOG = getattr(settings, 'NOMADBLOG_MULTIPLE_BLOGS', False)
 
 urlpatterns = patterns('',
     url(r'^$', NomadPageView.as_view(), name='home'),
+    url(r'^jobs/$', TemplateView.as_view(template_name="jobs.html"), name='jobs'),
+    url(r'^jobs/python/$', TemplateView.as_view(template_name="python_jobs.html"), name='python_jobs'),
     url(r'^contact/$', 'website.views.contact', name='contact'),
     url(r'^subscribe/$', SubscriberCreateView.as_view(), name='subscribe'),
     url(r'^blog/', include('nomadblog.urls')) if not MULTIBLOG else (r'^blogs/(?P<blog_slug>[-\w]+)/', include('nomadblog.urls')),
